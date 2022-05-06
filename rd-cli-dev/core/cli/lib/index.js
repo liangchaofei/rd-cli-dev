dd166463c071ed8e3cbdb9ec370b286e83dbb736
liangchaofei <chaofei_liang@126.com>
2022-05-06 17:37:52 +0800

    feat: 命令注册

--- rd-cli-dev/core/cli/lib/index.js
+++ rd-cli-dev/core/cli/lib/index.js
@@ -7,27 +7,61 @@ const semver = require('semver')
 const colors = require('colors/safe')
 const userHome = require('user-home')
 const pathExists = require('path-exists').sync;
+const commander = require('commander')
 const pkg = require('../package.json')
 const log = require('@rd-cli-dev/log')
 const { LOWEST_NODE_VERSION, DEFAULT_CLI_HOME } = require('./const')
 
 let args;
-
+const program = new commander.Command()
 async function core() {
     try{
         checkPkgVersion()
         checkNodeVersion()
         checkRoot()
         checkUserHome()
-        checkInputArgs()
+        // checkInputArgs()
         checkEnv()
         await checkGlobalUpdate()
-        log.verbose('debug', 'test debug log')
+        registryCommand()
     }catch(e){
         log.error(e.message)
     }
 }
 
+// 命令注册
+function registryCommand(){
+    program
+        .name(Object.keys(pkg.bin)[0])
+        .usage('<command> [options]')
+        .version(pkg.version)
+        .option('-d, --debug','是否开启调试模式', false);
+    
+    // 开启debug模式
+    program.on('option:debug', function(){
+        if(program._optionValues.debug){
+            process.env.LOG_LEVEL = 'verbose'
+        }else{
+            process.env.LOG_LEVEL = 'info'
+        }
+        log.level = process.env.LOG_LEVEL;
+        log.verbose('test')
+    })
+    // 对未知命令监听
+    program.on('command:*', function(obj){
+        console.log(obj)
+        const availableCommands = program.commands.map(cmd => cmd.name())
+        console.log(colors.red(`未知的命令：${obj[0]}`))
+        if(availableCommands.length > 0){
+            console.log(colors.red(`可用命令：${availableCommands.join(',')}`))
+        }
+    })
+    if(program.args && program.args.length < 1){
+        program.outputHelp()
+        console.log()
+    }
+    program.parse(process.argv)
+}
 // 检查是否要全局更新
 async function checkGlobalUpdate(){
     // 1.获取当前版本号和模块名
